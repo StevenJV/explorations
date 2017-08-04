d6eb183b8d37b4d6c248f873608c4af160ccfee4
StevenJV <StevenVore@tnwinc.com>
2017-08-04 12:59:54 -0400

    here's traditional Delegate syntax

--- LambdaExplorations/Program.cs
+++ LambdaExplorations/Program.cs
@@ -14,15 +14,32 @@ namespace LambdaExplorations
         private static void TraditionalDelegateSyntax(){
             List<int> list = new List<int>();
             list.AddRange(new int[] {20, 1, 4, 8, 9, 44});
+            Predicate<int> callback = IsEvenNumber;
+            List<int> evenNumbers = list.FindAll(callback);
+
+            PrintEvenNumbers(evenNumbers);
+        }
+
+        private static bool IsEvenNumber(int i){
+            return i % 2 == 0;
+        }
+
+
+        private static void AnonymousMethodSyntax(){
+            List<int> list = new List<int>();
+            list.AddRange(new int[] {20, 1, 4, 8, 9, 44});
 
             List<int> evenNumbers = list.FindAll(delegate(int i) { return (i%2) == 0; });
 
+            PrintEvenNumbers(evenNumbers);
+        }
+
+        private static void PrintEvenNumbers(List<int> evenNumbers){
             Console.WriteLine("even numbers:");
             foreach (int evenNumber in evenNumbers) {
                 Console.Write($"{evenNumber} \t");
             }
             Console.WriteLine();
-
         }
     }
 }
