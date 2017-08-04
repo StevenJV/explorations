263ef5c0fa97487c0cd71149c53ac78e612b88dd
StevenJV <StevenVore@tnwinc.com>
2017-08-04 12:52:15 -0400

    exploring Lambdas, start with a traditional delegate

---
+++ LambdaExplorations/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaExplorations
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Fun With Lambdas");
+            TraditionalDelegateSyntax();
+            Console.ReadKey();
+        }
+
+        private static void TraditionalDelegateSyntax(){
+            List<int> list = new List<int>();
+            list.AddRange(new int[] {20, 1, 4, 8, 9, 44});
+
+            List<int> evenNumbers = list.FindAll(delegate(int i) { return (i%2) == 0; });
+
+            Console.WriteLine("even numbers:");
+            foreach (int evenNumber in evenNumbers) {
+                Console.Write($"{evenNumber} \t");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
